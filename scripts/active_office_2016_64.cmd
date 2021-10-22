e170364ae1b593704549de1b3bd8e5e1a2bff50b
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2021-10-22 15:56:18 +0700

    feat: add active window office scripts

--- scripts/active_office_2016_64.cmd
+++ scripts/active_office_2016_64.cmd
@@ -1,7 +1,6 @@
+@echo off
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-
-@echo Please wait until the operation is done
 
 cd C:\Program Files\Microsoft Office\Office16
 cscript OSPP.VBS /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
@@ -10,5 +9,4 @@ cscript OSPP.VBS /act
 cscript OSPP.VBS /dstatus
 
 cls
-@echo Have a good day.
-@echo SaltyTuna
+echo "Have a good day. SaltyTuna"
