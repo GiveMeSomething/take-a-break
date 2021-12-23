f5d47834419320be0d35127b17be9afc519ef1b2
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2021-12-23 11:33:40 +0700

    refactor: remove Pomodoro

--- takeabreak.cmd
+++ takeabreak.cmd
@@ -9,7 +9,6 @@ rem This will be run when user enter wrong/invalid time
 :WRONGINPUT
 set time=
 set timeoption=
-set multiplier=1
 set realtime=
 set choice=
 set secondToHour=3600
@@ -73,52 +72,19 @@ set /a time=%time%
 
 if "%time%" == "1" (
 	set realtime=1800
-	goto NOTIMEOPTION
 )
 if "%time%" == "2" (
 	set realtime=2700
-	goto NOTIMEOPTION
 )
 if "%time%" == "3" (
 	set realtime=3600
-	goto NOTIMEOPTION
 )
 if "%time%" == "4" (
 	set realtime=5400
-	goto NOTIMEOPTION
 )
 if "%time%" == "5" (
 	set realtime=7200
-	goto NOTIMEOPTION
 )
-
-goto TIMEOPTION
-
-:TIMEOPTION
-
-set /p timeoption="Enter time options [M]inute | [S]econd (Default): "
-
-if "%timeoption%" == "M" (
-	set multiplier=60
-
-	@echo Time now calculate in MINUTE
-)
-if "%timeoption%" == "m" (
-	set multiplier=60
-
-	@echo Time now calculate in MINUTE
-) else (
-
-	@echo Time now calculate in SECOND
-
-)
-
-set /a timevalue = %time%
-set /a realtime=%time%*%multiplier%
-
-goto NOTIMEOPTION
-
-:NOTIMEOPTION
 
 set time=!realtime!
 
@@ -152,11 +118,11 @@ for /f "tokens=1*" %%A in ('
 
 timeout /nobreak !realtime!
 
-rundll32.exe user32.dll,LockWorkStation
+rem rundll32.exe user32.dll,LockWorkStation
 
 rem Ask the user if the script should continue
 :ASKCONTINUE
-set /p choice=Continue using[Y/N]?
+set /p choice="Continue using[Y/N]?"
 
 if "%choice%" == "Y" (goto YESOPTION)
 if "%choice%" == "y" (goto YESOPTION)
@@ -177,49 +143,8 @@ goto END
 :NOOPTION
 @echo Thanks for using this scripts ^
 
+
+:END
 This scripts will shutdown in 5 seconds
 timeout /nobreak 5
 exit
-
-rem Pomodoro feature. Will develop later
-:POMODOROSTART
-@echo ^
-
-This timer will run 4 times, 25 mins each, with 3 breaks ^
-
-Please take a big break after that ^
-
-
-
-set pomodorotime=0
-
-:POMODOROLOOP
-set /a numtime=pomodorotime + 1
-set pomodorotime=%numtime%
-
-@echo Starting loop number %pomodorotime%
-timeout /nobreak 1500
-start "" cmd /wait /c "echo End of loop&echo Please take a break&echo This window will automically close in 10 seconds&timeout 30& exit"
-
-if %pomodorotime% == 4 (goto POMODOROEND) else (goto POMODOROCHOICE)
-
-:POMODOROCHOICE
-set /p choice=Continue using[Y/N]?
-
-if "%choice%" == "Y" (goto POMODOROLOOP)
-if "%choice%" == "y" (goto POMODOROLOOP)
-
-if "%choice%" == "N" (goto POMODOROEND)
-if "%choice%" == "n" (goto POMODOROEND)
-
-:POMODOROEND
-@echo Congrats for completing a Pomodoro Cycle ^
-
-Please take a 20-30 mins break to refresh your mind :3 ^
-
-Computer will enter hibernate after 60 seconds
-timeout /nobreak 60
-shutdown -h
-
-:END
-cmd /k
